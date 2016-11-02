7dbaf8000c80351af38012608dcf97a943a11166
maev <jimenezolmos@yahoo.com>
2016-11-01 21:19:29 -0600

    after sylesheet

--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
-var React = require('react');
 var ReactDOM = require('react-dom');
+var routes = require('./config/routes')
 
-var HelloWorld = React.createClass({
-  render: function(){
-     return(
-       <div>Hello {this.props.name}</div>
-     )
-  }
-})
-
-ReactDOM.render(<HelloWorld name="Maeve"/>, document.getElementById('root'));
+ReactDOM.render(routes, document.getElementById('root'));
